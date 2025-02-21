37cc66c96811b27199f61677804bd26ccfd8c25d
Cfrant190 <CameronMFrantz@gmail.com>
2025-02-21 00:22:12 -0500

    added move method, movement and rotation active.

--- player.py
+++ player.py
@@ -31,3 +31,13 @@ class Player(CircleShape):
 
         if keys[pygame.K_d]:
             self.rotate(dt)
+
+    def move(self, dt):
+        keys = pygame.key.get_pressed()
+        forward = pygame.Vector2(0, 1).rotate(self.rotation)
+
+        if keys[pygame.K_w]:
+            self.position += forward * PLAYER_SPEED * dt
+
+        if keys[pygame.K_s]:
+            self.position += -forward * PLAYER_SPEED * dt
