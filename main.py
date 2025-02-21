cd4e10b0fd2ebbb79037e0d6fc85b811d1dd536f
Cfrant190 <CameronMFrantz@gmail.com>
2025-02-21 00:26:05 -0500

    movement fix

--- main.py
+++ main.py
@@ -13,9 +13,10 @@ def main():
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
     clock = pygame.time.Clock()
-    dt = clock.tick(60) / 1000
+    
     player = Player(SCREEN_WIDTH/2, SCREEN_HEIGHT/2)
     while True:
+        dt = clock.tick(60) / 1000
         for event in pygame.event.get():
 
             if event.type == pygame.QUIT:
