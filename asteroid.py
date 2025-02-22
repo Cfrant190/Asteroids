540c62b32357ac954fcddcc53a3df55cb5f456a0
Cfrant190 <CameronMFrantz@gmail.com>
2025-02-22 00:43:28 -0500

    changed ateroids to inherit from parent class constructor

--- asteroid.py
+++ asteroid.py
@@ -4,12 +4,13 @@ from circleshape import *
 
 class Asteroid(CircleShape):
     def __init__(self, x, y, radius):
+        super().__init__(x, y, radius)
         self.position = pygame.Vector2(x, y)
         self.velocity = pygame.Vector2(0, 0)
         self.radius = radius
 
     def draw(self, screen):
-        pygame.draw.circle(screen, (255, 255, 255), self.position, 2, width=2)
+        pygame.draw.circle(screen, (255, 255, 255), self.position, self.radius, width=2)
 
     def get_velocity(self):
         return self.velocity
--- main.py
+++ main.py
@@ -4,7 +4,7 @@
 import pygame
 from constants import *
 from player import *
-from asteroids import *
+from asteroid import *
 from asteroidfield import *
 
 def main():
